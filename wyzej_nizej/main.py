44c3cc818d8c1ba517270c9ccfbbefc396ab9091
unknown <tomaszek4411@gmail.com>
2021-01-31 12:35:20 +0100

    dzialajacy losowy wybor dodany

--- wyzej_nizej/main.py
+++ wyzej_nizej/main.py
@@ -1,20 +1,43 @@
 import random
 def main():
-    wyzej_nizej()
+    print(
+    """
+    Wyzej nizej:
+        Wybór losowy = 1
+        Wybór manualny = 2
+    """)
+    wybor = int(input())
+    if wybor == 1:
+        szukanie_losowe()
+    elif wybor == 2:
+        wyzej_nizej()
 def szukanie_losowe():
-    liczba1 = random.randint(0,1000)
-    liczba2 = random.randint(0,1000)
+    print("Komputer wybiera losową liczbe...")
+    liczba1 = random.randint(0,100)
+    print("Komputer wybrał liczbę: " + str(liczba1))
+    liczba2 = 10
+    mini = 0
+    maks = 100
     while(not liczba2 == liczba1):
 
         if liczba1 > liczba2:
-            message = "Nizej"
-            print(message)
-            liczba1 = int(input())
+            print("Nizej")
+            maks = liczba1
+            if mini == 0:
+                liczba1 = random.randint(0, maks +1)
+            else:
+                liczba1 = random.randint(mini, maks +1)
 
         elif liczba1 < liczba2:
-            message = "Wyzej"
-            print(message)
-            liczba1 = int(input())
+            print("Wyzej")
+            mini = liczba1
+            if maks == 100:
+                liczba1 = random.randint(mini, 100 +1)
+            else:
+                liczba1 = random.randint(mini, maks + 1)
+        print("Komputer wybrał liczbę: " + str(liczba1))
+
+    print("Udało się, ukryta liczba to liczba: " + str(liczba2))
 
 def wyzej_nizej():
     print("Podaj liczbe od 1 do 1000: ")
